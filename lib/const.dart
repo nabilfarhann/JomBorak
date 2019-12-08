07d6bb13af9cf6df56db103a3e7fd38c9e53f684
Nabil Farhan <nabilfarhan.dev@gmail.com>
2019-12-08 21:39:40 +0800

    initial commit

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+final themeColor = Color(0xfff5a623);
+final primaryColor = Color(0xFF212121);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xFF47CF73);
+final newThemeColor = Color(0xFF212121);
+final white = Color(0xFFFFFFFF);
